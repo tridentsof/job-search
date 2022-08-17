84549dd53c2084240136ff4b6998e75ad42561cf
trident <trident.prox@gmail.com>
2022-08-17 21:31:42 +0700

    Config eslint

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Trident is learning Vue" />
 </template>
 
 <script>
